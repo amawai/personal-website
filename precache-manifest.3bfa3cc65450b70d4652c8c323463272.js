cdf3a57f34cafc73d26b6a43120ff87b5acf16aa
Amanda <amawai@hotmail.com>
2019-01-20 19:19:07 -0500

    Updates

---
+++ precache-manifest.3bfa3cc65450b70d4652c8c323463272.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bcc43cdb0f7354d88134",
+    "url": "/static/js/main.bcc43cdb.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "bcc43cdb0f7354d88134",
+    "url": "/static/css/main.fbbc18d7.chunk.css"
+  },
+  {
+    "revision": "9d2f414bad9964942296f9e41c2fcbfb",
+    "url": "/index.html"
+  }
+];
