d070f5dd02a87bf46ec8d0b45854663e10f31fff
amawai <amawai@hotmail.com>
2021-02-11 22:17:11 -0500

    Small updates
    - Add README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # personal-website
+
+## Deployment
+
+1. Make sure the project builds
+2. Make sure that the `aws` cli tool is available
+3. Ensure that credentials + access codes are properly configured (`aws configure` if not)
+4. Run `npm run deploy` on this particular branch
--- src/App.js
+++ src/App.js
@@ -1,48 +1,56 @@
-import React, { Component } from 'react';
-import './App.css';
-import ParticlesContainer from './Components/ParticlesContainer';
-import GithubImage from './Images/github-10-xxl.png';
-import LinkedInImage from './Images/linkedin-4-xxl.png';
-import CvInfoImage from './Images/info-xxl.png';
+import React, { Component } from "react";
+import "./App.css";
+import ParticlesContainer from "./Components/ParticlesContainer";
+import GithubImage from "./Images/github-10-xxl.png";
+import LinkedInImage from "./Images/linkedin-4-xxl.png";
+import CvInfoImage from "./Images/info-xxl.png";
 
 class App extends Component {
   render() {
     return (
-      <div style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        background: "linear-gradient(45deg, rgb(14, 74, 112) 0%, rgb(0, 4, 7) 100%)"
-      }}>
-        <ParticlesContainer/>
-        <div
+      <div
         style={{
           position: "absolute",
           top: 0,
           left: 0,
           width: "100%",
-          height: "100%"
-        }}>
-          <div className='nametag'>
+          height: "100%",
+          background:
+            "linear-gradient(45deg, rgb(14, 74, 112) 0%, rgb(0, 4, 7) 100%)",
+        }}
+      >
+        <ParticlesContainer />
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <div className="nametag">
             AMANDA WAI
-            <p className='subtitle'>
-            Software Engineer,&nbsp;
-            Graduating May 2019
-            <p>
-              <a href="https://github.com/amawai">
-                <img height="40" width="40" src={GithubImage} alt="Github"/>
-              </a>
-              &nbsp;&nbsp;
-              <a href="https://www.linkedin.com/in/amandawai/">
-                <img height="40" width="40" src={LinkedInImage} alt="LinkedIn"/>
-              </a>
-              &nbsp;&nbsp;
-              <a href="https://drive.google.com/file/d/1kljmziZ-j1F48tW_bmyGIOlZkKdTMSAL/view">
-                <img height="40" width="40" src={CvInfoImage} alt="CV"/>
-              </a>
-            </p>
+            <p className="subtitle">
+              Software Engineer
+              <p>
+                <a href="https://github.com/amawai">
+                  <img height="40" width="40" src={GithubImage} alt="Github" />
+                </a>
+                &nbsp;&nbsp;
+                <a href="https://www.linkedin.com/in/amandawai/">
+                  <img
+                    height="40"
+                    width="40"
+                    src={LinkedInImage}
+                    alt="LinkedIn"
+                  />
+                </a>
+                &nbsp;&nbsp;
+                <a href="https://drive.google.com/file/d/1T7kKElS3Vtw3YZ15VCwMkVv1GojLkxvc/view?usp=sharing">
+                  <img height="40" width="40" src={CvInfoImage} alt="CV" />
+                </a>
+              </p>
             </p>
           </div>
         </div>
